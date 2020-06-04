820c64d43f7e022be1c28dfecf9eb16033cfb929
evilgamer163 <evilgamestm@gmail.com>
2020-06-04 09:39:56 +0400

    Домашнее задание первого урока.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let money = 0, //:(
+    income = 25000,
+    addExpenses = 1000,
+    deposit = '',
+    misson = 'Выучить JS!',
+    period = '8 недель';
+
+alert("Наша миссия на курсе: " + misson);
+
+console.log("Всего за " + period);
