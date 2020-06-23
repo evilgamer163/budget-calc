7150678870dcc56fdc0dfd71b07c62fdc3e04ac8
evilgamer163 <evilgamestm@gmail.com>
2020-06-23 18:34:29 +0400

    правки метода resetAll

--- js/main.js
+++ js/main.js
@@ -215,10 +215,23 @@ AppData.prototype.calcPeriod = function() {
 
 AppData.prototype.resetAll = function() {
     inputFields = document.querySelectorAll('input[type="text"]');
-    inputFields.forEach( item => {
-        item.removeAttribute('readonly');
-        item.value = '';
-    });
+        inputFields.forEach( item => {
+            item.removeAttribute('readonly');
+            item.value = '';
+        });
+
+        this.budget = 0;
+        this.income = {};
+        this.addIncome = [];
+        this.expenses = {};
+        this.addExpensens = [];
+        this.deposit = false;
+        this.budgetDay = 0;
+        this.budgetMonth = 0;
+        this.expensesMonth = 0;
+        this.statusIncome = '';
+        this.percentDeposit = 0;
+        this.moneyDeposit = 0;
 };
 
 const appData = new AppData();
